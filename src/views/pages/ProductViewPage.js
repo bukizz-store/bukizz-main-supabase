cf97ed085b82817f09817f62b14a9a4946ab9f9d
Shivam-043 <12111043@nitkkr.ac.in>
2026-03-31 21:11:01 +0530

    fix: simplify delivery message in ProductViewPage

--- src/views/pages/ProductViewPage.js
+++ src/views/pages/ProductViewPage.js
@@ -1052,7 +1052,7 @@ function ProductViewPage() {
                         className="w-5 h-5 text-gray-600 opacity-70"
                       />
                       <p className="text-sm font-semibold text-gray-800">
-                        Delivery by Tomorrow, 11 AM - 1 PM
+                        Delivery by Tomorrow
                       </p>
                     </>
                   ) : (
